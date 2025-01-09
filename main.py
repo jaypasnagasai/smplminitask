43d6850b08bc3b09daeda07666199cd22b662f30
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2025-01-08 20:47:44 -0800

    Update main.py

--- main.py
+++ main.py
@@ -15,14 +15,3 @@ for key in data.files:
     print(f"\nData for key '{key}':")
     print(data[key])
     print("Shape:", data[key].shape)
-
-# Close the file (optional but good 
-
-
-
-# Week 2 Agenda
-# Finish the mini task
-# Figure out what 156 corresponds to
-# Figure out the angles
-# Download the entire dataset
-# Text Descriptions for each [Human3D]
