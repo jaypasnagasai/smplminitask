eb823a7f77c28b87ee5f6444b2dd64982d3d1e62
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2025-01-08 20:58:42 -0800

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,21 +1,20 @@
 import numpy as np
-import pandas as pd
 
 # Load the NPZ file
 file_path = 'stand.npz'  # Replace with your file path
 data = np.load(file_path)
 
 # Extract the 'poses' data
-poses_data = data['poses']
+poses = data['poses']
 
 # Define the indices for joint [4]
 joint_index = 4
-values_per_joint = 3  # Each joint has 3 dimensions (e.g., X, Y, Z)
-start_idx = joint_index * values_per_joint
-end_idx = start_idx + values_per_joint
+values_per_joint = 3  # Each joint has 3 dimensions ( X, Y, Z)
+start_idx = joint_index * values_per_joint # 12
+end_idx = start_idx + values_per_joint - 1 # 14
 
 # Extract pose data for joint [4]
-joint_4_data = poses_data[:, start_idx:end_idx]
+joint_4_data = poses[:, start_idx:end_idx]
 
 # Save the joint 4 data to an NPZ file
 output_npz_path = 'joint_4_pose_data.npz'  # Replace with your desired output path
